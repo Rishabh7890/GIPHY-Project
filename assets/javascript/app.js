94a03bb0e90e0b11377b13a7229ccb128fa93fd9
Rishabh Goel <Rishabh@rishabhs-mbp.lan>
2019-04-07 19:37:59 -0400

    v2

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,18 @@
-//create vars for starter recommended GIFs 
-var gifs = ["Funny", "Dogs", "Cats", "Sports", "Movies", "Anime", "Music","Art","gaming","Cities","Holiday","Politics"];
+//create empty string for gifs to be added
+var gifs = [""];
 
 //function displayGifInfo re-renders HTML to display content 
-$("button").on("click", function(){
+function displayMovieInfo() {
+
   var gif = $(this).attr("data-gif");
-  var apiKey ="&api_key=ATitLzqHisbzyItV8xmCrCqEKbGnqMYM";
-  var queryURL = "http://api.giphy.com/v1/gifs/search?q" + gif + apiKey + "&limit=10";
+  var apiKey = "&api_key=ATitLzqHisbzyItV8xmCrCqEKbGnqMYM";
+  var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gif + apiKey + "&limit=10";
 
   //Create AJAX call for specific gif button being clicked 
   $.ajax({
     url: queryURL,
     method: "GET"
-  }).then(function(response){
+  }).then(function (response) {
     console.log(response);
 
     //store data from the AJAX request in a results variable
@@ -22,18 +23,48 @@ $("button").on("click", function(){
 
       var gifDiv = $("<div>");
 
-      var gifRating = $("<p>").text("Rating: " + results[i].rated);
+      var gifRating = $("<p>").text("Rating: " + results[i].rating);
 
       var gifImage = $("<img>");
 
       gifImage.attr("src", results[i].images.fixed_height.url);
 
+      gifDiv.append(gifImage);
       gifDiv.append(gifRating);
-      gifDiv.append(gifImage)
 
       $("#gifs-view").prepend(gifDiv);
     }
-})
+  })
+}
+
+function renderButtons () {
+
+  $("buttons-view").empty();
+
+  for (var i = 0; i < gifs.length; i++) {
+
+    var newBtn = $("<button>");
+    newBtn.addClass("gif");
+    newBtn.attr("data-gif", gifs[i]);
+    newBtn.text(gifs[i]);
+    $("buttons-view").append(newBtn);
+  }
+}
+
+$(".add-gif").on("click",function(event) {
+  event.preventDefault();
+
+  var gif = $("#gif-input").val().trim();
+
+  gifs.push(gif);
+
+  renderButtons();
 })
 
+$(document).on("click", ".add-gif", displayMovieInfo);
 
+renderButtons();
+
+
+//Errors with code: 
+// Same gifs print everytime no matter which button is pressed
